610ae7d25ecfbaa3388b7701b6b1aaf983064340
georgecode <georgeczurlanis@gmail.com>
2019-02-04 16:54:06 -0800

    Add react clean up boiler plate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>App.js</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//<img src={logo} className="App-logo" alt="logo" />
